17fed759f021a01931c7f06b49225d9ccd23e93c
Nacho Fernandez Peralta <66188006+ignaciofpqr@users.noreply.github.com>
2021-08-03 15:23:33 -0300

    Adding JPA. Converting Get, Post and Delete methods.

--- src/main/java/com/in28minutes/restwebservice/user/User.java
+++ src/main/java/com/in28minutes/restwebservice/user/User.java
@@ -3,14 +3,20 @@ package com.in28minutes.restwebservice.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2)
@@ -19,6 +25,10 @@ public class User {
 
     @Past
     private Date birthDate;
+
+    public User() {
+
+    }
 
     @Override
     public String toString() {
